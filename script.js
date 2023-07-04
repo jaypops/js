1534ad76db84445792ddcdf60f1b5b0e6bf56cfb
Timothy-TO <73559338+jaypops@users.noreply.github.com>
2023-07-04 14:16:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+//if statements for voting
+
+// const can = ["a", "b", "c"];
+// const re = prompt("You can only vote for one canditate (a,b,c)");
+// if (re === "a" || re === "b" || re === "c") {
+//   alert(`you voted for ${re}`);
+// } else {
+//   alert(`try again`);
+// }
+
+//if statements for odd and even number
+
+// const number = prompt("fill in your number");
+// if (number % 2 == 0) {
+//   alert("number is even");
+// } else {
+//   alert("number is odd");
+// }
+
+//if statements for numbers is divisible by 7
+// const numS = prompt("numbers divisible by 7:");
+// if (Number(String(numS)) % 7 === 0) {
+//   alert(`${numS} is divisible by 7`);
+// } else {
+//   alert(`${numS} is not divisible by 7`);
+// }
+
+//if statements for multiples of 5
+// const mult = prompt("input number :");
+// if (Number(String(mult)) % 5 === 0) {
+//   alert("Hello");
+// } else {
+//   alert("Bye");
+// }
