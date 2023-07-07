f2457e4fa1e72144a2c6d58d7939ba5400c7854b
Timothy-TO <73559338+jaypops@users.noreply.github.com>
2023-07-07 06:13:12 +0100

    Update voting.js

--- voting.js
+++ voting.js
@@ -1,8 +1,8 @@
 //if statements for voting
 
 function vote(vot) {
-  console.log("You can only vote for one canditate (a,b,c):");
   for (const re of vot) {
+      console.log("You can only vote for one canditate (a,b,c):");
     if (re === "a" || re === "b" || re === "c") {
       console.log(`you voted for ${re}`);
     } else {
