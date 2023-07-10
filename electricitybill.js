710a882871302eedb3552f99a58433709dd1cbde
Timothy-TO <73559338+jaypops@users.noreply.github.com>
2023-07-10 01:06:45 +0100

    Add files via upload

---
+++ electricitybill.js
@@ -0,0 +1,12 @@
+function cal(bills) {
+  if (bills <= 100) {
+    console.log("No charges");
+  }
+  if ((bills = 100 || bills >= 200)) {
+    console.log("5 naria par charges");
+  }
+  if (bills >= 200) {
+    console.log("10 natia par charges");
+  }
+}
+cal(100);
