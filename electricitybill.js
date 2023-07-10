f575567d40ad5492c79173425bd211b3bdc8376c
Timothy-TO <73559338+jaypops@users.noreply.github.com>
2023-07-10 01:07:42 +0100

    Update electricitybill.js

--- electricitybill.js
+++ electricitybill.js
@@ -1,12 +1,14 @@
 function cal(bills) {
-  if (bills <= 100) {
-    console.log("No charges");
+  if (bills < 100) {
+    console.log("no charge");
   }
-  if ((bills = 100 || bills >= 200)) {
-    console.log("5 naria par charges");
+  if (bills >= 100 && bills < 200) {
+    console.log("5 naria per unit");
   }
   if (bills >= 200) {
-    console.log("10 natia par charges");
+    console.log("10 naria per unit");
   }
 }
-cal(100);
+cal(40);
+cal(500);
+cal(150);
